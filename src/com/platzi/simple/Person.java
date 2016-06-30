5d10b84ace660cf7db0046b552719467ac9e7f7c
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-06-30 22:27:01 +0200

    create Person class, init instance variables

---
+++ src/com/platzi/simple/Person.java
@@ -0,0 +1,18 @@
+package com.platzi.simple;
+
+/**
+ * Simple object
+ */
+public class Person {
+    private byte year;
+    private byte height;
+    private short monthSalary;
+    private int yearSalary;
+
+    public Person(byte year, byte height, short monthSalary, int yearSalary) {
+        this.year = year;
+        this.height = height;
+        this.monthSalary = monthSalary;
+        this.yearSalary = yearSalary;
+    }
+}
