20f43d28a777c1ef4bac88af4914695294d23b86
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-07-02 11:38:13 +0200

    validation state years for persons

--- src/com/platzi/simple/Person.java
+++ src/com/platzi/simple/Person.java
@@ -4,15 +4,35 @@ package com.platzi.simple;
  * Simple object
  */
 public class Person {
-    private byte year;
+    public final byte ADULT_YEARS = 18;
+    public final byte RETIREMENT_YEARS = 65;
+
+    private byte years;
     private byte height;
     private short monthSalary;
     private int yearSalary;
 
-    public Person(byte year, byte height, short monthSalary, int yearSalary) {
-        this.year = year;
+    public Person(byte years, byte height, short monthSalary, int yearSalary) {
+        this.years = years;
         this.height = height;
         this.monthSalary = monthSalary;
         this.yearSalary = yearSalary;
+    }
+
+    public byte getYearsToRetirement() {
+        return getValidateYears(RETIREMENT_YEARS);
+    }
+
+    public byte getYearsToBeAdult() {
+        return getValidateYears(ADULT_YEARS);
+    }
+
+    public byte getYearsOfWorkQuote() {
+        return RETIREMENT_YEARS - ADULT_YEARS;
+    }
+
+    private byte getValidateYears(byte yearValidation) {
+        byte validYears = (byte) (yearValidation - this.years);
+        return validYears <= 0 ? -1 : validYears;
     }
 }
