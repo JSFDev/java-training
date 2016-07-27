ff00ceacdf7f29bb76a5de691bf7a3d5dc52eca9
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2016-07-27 21:03:02 +0200

    create test service : getInstructorCourses

--- src/test/com/platzi/appRestCoursesInstructors/services/CourseServiceTest.java
+++ src/test/com/platzi/appRestCoursesInstructors/services/CourseServiceTest.java
@@ -11,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
@@ -18,11 +20,14 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class CourseServiceTest extends TestCase {
     private InstructorEntity instructor;
-    private CourseEntity course;
-    private Long courseId = (long) 12345;
-    private String userName = "username";
-    private String password = "password";
-    private String courseDescription = "course description";
+    private Long course1Id;
+    private String userName;
+    private String password;
+    private Collection<CourseEntity> courseList;
+    private CourseEntity course1;
+    private CourseEntity course2;
+    private String courseDescription1;
+    private String courseDescription2;
 
     @Mock
     CourseRepository courseRepository;
@@ -36,17 +41,37 @@ public class CourseServiceTest extends TestCase {
     @InjectMocks
     InstructorService instructorService;
 
-    private void createMockInstructorAndCourse() {
+    private void initializeInstructorAndCourse() {
+        course1Id = (long) 12345;
+        userName = "username";
+        password = "password";
+        courseDescription1 = "course description one";
+        courseDescription2 = "course description two";
         instructor = new InstructorEntity(this.userName, this.password);
-        course = new CourseEntity(this.instructor, this.courseDescription);
-        when(instructorRepository.findByUsername(this.userName)).thenReturn(Optional.of(this.instructor));
-        when(courseRepository.findOne(courseId)).thenReturn(course);
+        course1 = new CourseEntity(this.instructor, this.courseDescription1);
+        course2 = new CourseEntity(this.instructor, this.courseDescription2);
+        courseList = Arrays.asList(course1, course2);
     }
 
     @Test
-    public void shouldReadCourseFromUserName() {
-        this.createMockInstructorAndCourse();
-        CourseEntity restCourse = courseService.getInstructorCourseById(this.userName, courseId);
-        assertEquals(course.getDescription(), restCourse.getDescription());
+    public void shouldReadCourseFromUserNameAndCourseId() {
+        CourseEntity restCourse;
+
+        this.initializeInstructorAndCourse();
+        when(instructorRepository.findByUsername(this.userName)).thenReturn(Optional.of(this.instructor));
+        when(courseRepository.findOne(course1Id)).thenReturn(course1);
+        restCourse = courseService.getInstructorCourseById(this.userName, this.course1Id);
+        assertEquals(course1.getDescription(), restCourse.getDescription());
+    }
+
+    @Test
+    public void shouldReadAllCoursesFromUserNameAndCourseId() {
+        Collection<CourseEntity> restCourse;
+
+        this.initializeInstructorAndCourse();
+        when(instructorRepository.findByUsername(this.userName)).thenReturn(Optional.of(this.instructor));
+        when(courseRepository.findByInstructorUsername(this.userName)).thenReturn(courseList);
+        restCourse = courseService.getInstructorCourses(userName);
+        assertEquals(courseList, restCourse);
     }
 }
